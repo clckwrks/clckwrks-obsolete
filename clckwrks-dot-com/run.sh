e23b1408ce163f86d4b25f018f2c8559b5e07ca9
Jeremy Shaw <jeremy@seereason.com>
2012-06-14 20:12:55 +0000

    clckwrks-theme-clckwrks-0.1.19: more theme hacks

--- clckwrks-dot-com/run.sh
+++ clckwrks-dot-com/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-runhaskell -i../clckwrks/ -i../clckwrks-theme-clckwrks/ -i../clckwrks-plugin-media -i../clckwrks-plugin-bugs Main.hs --http-port 8000 --jstree-path=../jstree --json2-path=../json2 --jquery-path=../jquery --bugs-data-path=../clckwrks-plugin-bugs/data "$1" "$2"
+runhaskell -i../clckwrks/ -i../clckwrks-theme-clckwrks/ -i../clckwrks-plugin-media -i../clckwrks-plugin-bugs Main.hs --jstree-path=../jstree --json2-path=../json2 --jquery-path=../jquery --bugs-data-path=../clckwrks-plugin-bugs/data "$1" "$2" "$3" "$4"
--- clckwrks-theme-clckwrks/Theme/Home.hs
+++ clckwrks-theme-clckwrks/Theme/Home.hs
@@ -17,23 +17,28 @@ summaryBox pid title iconURL =
 page :: XMLGenT (Clck ClckURL) XML
 page =
     template' "clckwrks.com" () $
-        <%>
+        <div id="homepage">
+
+         <div id="page-content">
+         <h1>clckwrks</h1>
+{-
          <div id="logo">
           <img src=(ThemeData "clckwrks-logo.png") />
          </div>
-
+-}
          <blockquote>
 --          <p><span class="big-quote">“</span>runs smoothly and invisibly<span class="big-quote">”</span> - <span class="quote-author">Katherine Durkes</span></p>
           <p><span class="big-quote">“</span>An open-source CMS you can trust, built with the dexterity of Haskell.<span class="big-quote">”</span></p>
          </blockquote>
 
-         <div id="page-content">
           <% getPageContent %>
-         </div>
 
-         <div class="summary-boxes">
+          <div class="summary-boxes">
            <% summaryBox (PageId 8) "Why?" "philosophy-icon.png" %>
            <% summaryBox (PageId 3) "Get Started" "7-icon.png" %>
            <% summaryBox (PageId 2) "Get Involved" "8-icon.png" %>
          </div>
-        </%>
+
+         </div>
+
+        </div>
